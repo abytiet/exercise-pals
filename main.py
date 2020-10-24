1db5637f5d10717e45aba0f591f535dfced23146
aby <abigailtiet@gmail.com>
2020-10-24 00:07:40 -0400

    submit form, need chat

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_socketio import SocketIO
+from form import UserInfo
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5b763eaf5b92e9fc20bde203c1c4f1cb'
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def home():
-    return render_template("index.html")
+    form = UserInfo()
+    if form.validate_on_submit():
+        print("success")
+    return render_template("index.html", form=form)
+
+@app.route('/chat')
+def chat():
+    username = request.args.get('username')
+    if username:
+        return render_template("chat.html", username=username)
+    else:
+        redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
