2abb7bc9a9a6b2123b8a77cef00fa1c66e219a46
aby <abigailtiet@gmail.com>
2020-10-24 16:23:46 -0400

    base chat functionality

--- form.py
+++ form.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class UserInfo(FlaskForm):
     username = StringField('username', validators=[DataRequired(), Length(min=3, max=20)])
+    workout = SelectField('workout', validators=[DataRequired(), Length(min=3, max=20)], choices=[('Cardio'), ('Strength'), ('Flexibility')])
     submit = SubmitField('go!')
--- main.py
+++ main.py
@@ -1,27 +1,58 @@
 from flask import Flask, render_template, request, redirect, session
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, leave_room, send
 from form import UserInfo
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 app.config['SECRET_KEY'] = '5b763eaf5b92e9fc20bde203c1c4f1cb'
 
 @app.route('/', methods=['GET','POST'])
 def home():
     form = UserInfo(request.form)
     if form.validate_on_submit():
-        print(request.form.get('username'))
+        #print(request.form.get('username'))
+        #print(request.form.get('workout'))
         session['username'] = request.form.get('username')
+        session['workout'] = request.form.get('workout')
         return redirect("/chat")
     return render_template("index.html", form=form)
 
 @app.route('/chat')
 def chat():
     username = session.get('username')
-    print(username)
-    if username is not None:
-        return render_template("chat.html", username=username)
+    workout = session.get('workout')
+    #print(username)
+    #print(workout)
+    if (username is not None) and (workout is not None):
+        return render_template("chat.html", username=username, title=workout, workout=workout)
     else:
         return redirect('/')
 
+
+@socketio.on('join_room')
+def handle_on_join(data):
+    username = data['username']
+    workout = data['workout']
+    #app.logger.info('{} has joined room {}'.format(username, workout))
+    join_room(workout)
+    socketio.emit('join_room_announcement', data, room=workout)
+
+
+@socketio.on('send_message')
+def handle_send_message(data):
+    username = data['username']
+    workout = data['workout']
+    message = data['message']
+    #app.logger.info('{} sent message when doing {}: {} '.format(username, workout, message))
+    socketio.emit('receive_message', data, room=workout)
+
+@socketio.on('leave_room')
+def handle_leave_room_event(data):
+    username = data['username']
+    workout = data['workout']
+    #app.logger.info("{} has left the room {}".format(username, workout))
+    leave_room(workout)
+    socketio.emit('leave_room_announcement', data, room=workout)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
