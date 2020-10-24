516d6855d763cdedb02e53a2cac8c163ddebfa9a
aby <abigailtiet@gmail.com>
2020-10-24 01:06:55 -0400

    session thing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from flask_socketio import SocketIO
 from form import UserInfo
 
@@ -7,16 +7,18 @@ app.config['SECRET_KEY'] = '5b763eaf5b92e9fc20bde203c1c4f1cb'
 
 @app.route('/', methods=['GET','POST'])
 def home():
-    form = UserInfo()
+    form = UserInfo(request.form)
     if form.validate_on_submit():
-        print(request.args.get('username'))
+        print(request.form.get('username'))
+        session['username'] = request.form.get('username')
         return redirect("/chat")
     return render_template("index.html", form=form)
 
 @app.route('/chat')
 def chat():
-    username = request.args.get('username')
-    if username:
+    username = session.get('username')
+    print(username)
+    if username is not None:
         return render_template("chat.html", username=username)
     else:
         return redirect('/')
